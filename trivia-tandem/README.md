f55b8ee6219a5ebb11c56e0e739d23bc338d29e5
Cade Schreger <cschreger@gmail.com>
2020-11-01 18:59:27 -0500

    Done pre-README

--- trivia-tandem/README.md
+++ trivia-tandem/README.md
@@ -1,6 +1,30 @@
-# Getting Started with Create React App
+# Tandem Trivia
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Welcome to Tandem Trivia - a single-page React application created as part of Tandem's application process!
+
+Challenge Prompt: 
+
+# Instructions
+
+Steps 1-2:
+Clone/Download repo
+Open terminal and cd into root project folder(trivia-tandem)
+If package.json exists, delete package.json
+
+Steps 3-4:
+Run the following commands:
+    npm install
+    npm start
+
+Step 5:
+Enjoy and good luck!
+
+# Technologies/Libraries:
+
+React.js
+JavaScript
+HTML5
+CSS3
 
 ## Available Scripts
 
@@ -39,32 +63,3 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- trivia-tandem/src/quiz.js
+++ trivia-tandem/src/quiz.js
@@ -25,10 +25,12 @@ export class Quiz extends Component {
 
     answerShuffle(choices, correct) {
         let options = choices.concat(correct);
+
         for (let i = options.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [options[i], options[j]] = [options[j], options[i]];
         }
+
         return options;
     }
 
@@ -48,34 +50,26 @@ export class Quiz extends Component {
 
 
     update = () => {
-        let choices = document.getElementsByClassName('choice')
-        debugger
+        let choices = document.getElementsByClassName('choice');
+
         Array.from(choices).forEach(selection => {
             if (selection.innerHTML === this.state.answer){
                 selection.setAttribute("style", 'background-color: #71d5f3;')
                 selection.setAttribute('class', 'choice')
             }
         })
-        debugger
-        if (this.state.questionNum === 2){
-            let appender = document.getElementById('total-score')
-            let newGame = document.createElement('button')
-            appender.appendChild(newGame)
-            newGame.innerHTML = 'New Game'
-            newGame.className = 'new-game'
-            newGame.addEventListener('click', this.loadQuiz())
-        } else {
-            this.setState({
-                currIndex: this.state.currIndex += 1,
-                questionNum: this.state.questionNum += 1,
-                question: quizQuestions[this.state.currIndex].question,
-                options: this.answerShuffle(
-                    quizQuestions[this.state.currIndex].incorrect, 
-                    quizQuestions[this.state.currIndex].correct
-                ),
-                answer: quizQuestions[this.state.currIndex].correct
-            })
-        }
+
+        this.setState({
+            currIndex: this.state.currIndex += 1,
+            questionNum: this.state.questionNum += 1,
+            question: quizQuestions[this.state.currIndex].question,
+            options: this.answerShuffle(
+                quizQuestions[this.state.currIndex].incorrect, 
+                quizQuestions[this.state.currIndex].correct
+            ),
+            answer: quizQuestions[this.state.currIndex].correct
+        })
+    
 
         if (this.state.questionNum === 11){
             this.setState({
@@ -90,21 +84,21 @@ export class Quiz extends Component {
         const {answer} = this.state;
 
         if (choice === answer) {
-            let choices = document.getElementsByClassName('choice')
-            debugger
+            let choices = document.getElementsByClassName('choice');
+
             Array.from(choices).forEach(selection => {
                 if (selection.innerHTML === answer){
                     selection.setAttribute("style", 'background-color: green;')
                 }
             })
-            debugger
+
             this.setState ({
                 score: this.state.score += 1,
                 roundScore: this.state.roundScore += 1
             })
         } else {
             let choices = document.getElementsByClassName('choice')
-            debugger
+
             Array.from(choices).forEach(selection => {
                 if (selection.innerHTML === answer){
                     selection.setAttribute("style", 'background-color: green;')
@@ -117,34 +111,64 @@ export class Quiz extends Component {
     }
 
 
+    newGame = () => {
+        const {currIndex} = this.state;
+
+        this.setState({
+            currIndex: 0,
+            questionNum: 1,
+            roundScore: 0,
+            round: 1,
+            score: 0,
+            question: quizQuestions[currIndex].question,
+            options: this.answerShuffle(
+                    quizQuestions[currIndex].incorrect, 
+                    quizQuestions[currIndex].correct
+                    ),
+            answer: quizQuestions[currIndex].correct
+        })
+    }
+
+    
+
     render() {
         const {options, question, score, roundScore, round, questionNum} = this.state;
 
-        return (
-        <div className='question-wrapper'>
-            <h2>Round {round}: Question {`${questionNum > 10 ? questionNum - 10 : questionNum}`} of 10</h2>
-            <h3>{question}</h3>
-
-            <div className='options-wrapper'>
-                <p onClick={this.handleNextQuestion.bind(null, options[0])} className='choice'>{options[0]}</p>
-                <p onClick={this.handleNextQuestion.bind(null, options[1])} className='choice'>{options[1]}</p>
-                <p onClick={this.handleNextQuestion.bind(null, options[2])} className='choice'>{options[2]}</p>
-                <p onClick={this.handleNextQuestion.bind(null, options[3])} className='choice'>{options[3]}</p>
+        if (this.state.questionNum === 21) {
+            return (
+                <div className='new-game-wrapper'>
+                <div>Thank you for playing!</div>
+                <div>Round 1 Score: {this.state.score - this.state.roundScore}</div>
+                <div>Round 2 Score: {this.state.roundScore}</div>
+                <div>Final Score: {this.state.score} / 20</div>
+                    <button className= 'new-game' onClick={() => this.newGame()}>New Game</button>
+                </div>
+            )
+        } else {
+            return (
+            <div className='question-wrapper'>
+                <h2>Round {round}: Question {`${questionNum > 10 ? questionNum - 10 : questionNum}`} of 10</h2>
+                <h3>{question}</h3>
+            
+                <div className='options-wrapper'>
+                    <p onClick={this.handleNextQuestion.bind(null, options[0])} className='choice'>{options[0]}</p>
+                    <p onClick={this.handleNextQuestion.bind(null, options[1])} className='choice'>{options[1]}</p>
+                    <p onClick={this.handleNextQuestion.bind(null, options[2])} className='choice'>{options[2]}</p>
+                    <p onClick={this.handleNextQuestion.bind(null, options[3])} className='choice'>{options[3]}</p>
+                </div>
+            
+                <div className='round-score'>
+                    <span>Round {round} Score: </span>
+                    <span>{roundScore}</span>
+                </div>
+            
+                <div id ='total-score' className='total-score'>
+                    <span>Total Score: </span>
+                    <span>{score}</span>
+                </div>
             </div>
-        
-            <div className='round-score'>
-                <span>Round {round} Score: </span>
-                <span>{roundScore}</span>
-            </div>
-
-            <div id ='total-score' className='total-score'>
-                <span>Total Score: </span>
-                <span>{score}</span>
-            </div>
-
-        </div>
-        )
-    }
+            )
+    }}
 }
 
 
